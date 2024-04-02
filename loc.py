218a5aa1987881dfefbeb5e6e1b79582e23a1e24
Jelte2357 <86104076+Jelte2357@users.noreply.github.com>
2024-04-02 13:24:12 +0200

    Update
    Still does not work in cmd, only in powershell

--- loc.py
+++ loc.py
@@ -1,32 +1,30 @@
 # this program will be run from the command line. It should take a single argument, a command. The option -o should also be possible.
-import rich
 from rich.console import Console
 from rich import print
 import sys
 import os
 import argparse
-
+from pathlib import WindowsPath
 console = Console()
 
 # program can either be run "loc {command}" or "loc -o {command}"
 
 def main():
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("command", type=str, help="The command to run")
-    parser.add_argument("-o", "--open", action="store_true", help="Open the command in the default program")
+    parser.add_argument("-o", "--open", action="store_true")
+    parser.add_argument("command", type=str)
     args = parser.parse_args()
     # to run this, you need to have which installed. I use github's version.
-    location = os.popen(f"which {args.command}").read()
-    if f"no {args.command} in" in location:
+    location = os.popen(f"where {args.command}").read()
+    if f"INFO: Could not find files for the given pattern(s)." in location:
         print(f"Command '{args.command}' not found")
         sys.exit(1)
     
-    # this is not great, I think this also only works on the C:/ drive. Will possibly fix later.
-    location = os.path.abspath(os.path.dirname(location)[2:])
+    location = WindowsPath(location.strip()) 
     
     if args.open:
-        os.system(f"explorer {location}")
+        os.system(f"explorer {location.parent.absolute()}")
     else:
-        print(f"[link=file://{location}]{location}[/link]", end="")
+        print(f"[link=file://{location.parent.absolute()}]{location.parent.absolute()}[/link]")
     
 main()
