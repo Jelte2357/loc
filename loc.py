cc746339cc58459f852a2a3f43fdc627a59a3a76
Jelte2357 <86104076+Jelte2357@users.noreply.github.com>
2024-04-10 14:22:05 +0200

    Better, Now with -py

--- loc.py
+++ loc.py
@@ -1,30 +1,45 @@
 # this program will be run from the command line. It should take a single argument, a command. The option -o should also be possible.
-from rich.console import Console
 from rich import print
 import sys
-import os
+from subprocess import Popen, PIPE
 import argparse
 from pathlib import WindowsPath
-console = Console()
+import os
 
 # program can either be run "loc {command}" or "loc -o {command}"
+# you can also get python modules' locations by running "loc -py {module}"
 
 def main():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument("-o", "--open", action="store_true")
+    parser.add_argument("-py", "--python", action="store_true")
     parser.add_argument("command", type=str)
     args = parser.parse_args()
-    # to run this, you need to have which installed. I use github's version.
-    location = os.popen(f"where {args.command}").read()
-    if f"INFO: Could not find files for the given pattern(s)." in location:
-        print(f"Command '{args.command}' not found")
-        sys.exit(1)
+    if args.python:
+        try:
+            location = WindowsPath(__import__(args.command).__file__).parent.absolute()
+            if args.open:
+                os.system(f"explorer {location}")
+            else:
+                print(f"[link=file://{location.as_uri()}]{location}[/link]")
+            
+            
+        except ImportError:
+            print(f"Module '{args.command}' not found")
+            sys.exit(1)
     
-    location = WindowsPath(location.strip()) 
-    
-    if args.open:
-        os.system(f"explorer {location.parent.absolute()}")
     else:
-        print(f"[link=file://{location.parent.absolute()}]{location.parent.absolute()}[/link]")
-    
+        # to run this, you need to have which installed. I use github's version.
+        location = Popen(f"where {args.command}".split(), stdout=PIPE, stderr=PIPE).communicate()[0].decode()
+        if not location:
+            print(f"Command '{args.command}' not found")
+            sys.exit(1)
+
+        location = WindowsPath(location.strip()) 
+        
+        if args.open:
+            os.system(f"explorer {location.parent.absolute()}")
+        else:
+            print(f"[link=file://{location.parent.absolute().as_uri()}]{location.parent.absolute()}[/link]")
+        
 main()
